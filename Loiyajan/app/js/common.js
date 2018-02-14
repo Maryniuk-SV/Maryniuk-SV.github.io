d5f8edceed9dc3aeabc417ae0e5ad601808fe530
Maryniuk-SV <mefissto.w@gmail.com>
2018-02-14 22:22:37 +0200

    fix bugs in styles

--- Loiyajan/app/js/common.js
+++ Loiyajan/app/js/common.js
@@ -3,30 +3,45 @@ $(function(){
         loop:true,
         dots:false,
         autoWidth:true,
-    // items:5,
-    margin:50,
-    responsiveClass: true,
-    nav:true,
-    navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
+        margin:50,
+        responsiveClass: true,
+        nav:true,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
         }
-    }
-})
+    })
 
     $('.up__btn').click(function() {
         $('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
     });
 
+    $('.foto__wrap').mouseover(
+     function(){ $(this).addClass('team__hover') }
+     );
+    $('.foto__wrap').mouseout(
+     function(){ $(this).removeClass('team__hover') }
+     );
+    $('.social').mouseover(
+     function(){ $(this).prev().addClass('team__hover') }
+     );
+    $('.social').mouseout(
+     function(){ $(this).prev().removeClass('team__hover') }
+     );
 
+    $('.show_right_menu').click(function(){
+        $('.menu_right').toggleClass('drop-down show')
+    });
+    $('.show_menu').click(function(){
+        $('.menu').toggleClass('drop-down show')
+    });
 
-
-    
-})
+});
